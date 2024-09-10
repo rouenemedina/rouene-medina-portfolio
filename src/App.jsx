2728ba0f307089cb6b6573159bcc843612a14f1d
rouenemedina <rouenemedina@gmail.com>
2024-09-10 06:45:37 -0400

    created Routes in App.jsx, and created a HomePage with boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.scss";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import "./HomePage.scss";
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <main>
+            
+        </main>
+    );
+};
+
+export default HomePage;
