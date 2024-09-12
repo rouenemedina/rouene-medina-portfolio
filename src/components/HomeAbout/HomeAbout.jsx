310a8e0d05ffa61d0ab995b600dbb53120a1b67f
rouenemedina <rouenemedina@gmail.com>
2024-09-12 19:35:00 -0400

    added placeholder for text in about component

--- src/components/HomeAbout/HomeAbout.jsx
+++ src/components/HomeAbout/HomeAbout.jsx
@@ -3,13 +3,25 @@ import React from "react";
 const HomeAbout = () => {
   return (
     <main className="about">
-        <section>
+      <section>
+        <article>
           <h2>We All Have a Story</h2>
           <h3>Here's mine</h3>
-        </section>
-        <section>
-          <img></img>
-        </section>
+        </article>
+        <p>
+          In molestie nisl ac volutpat fermentum. Fusce sed volutpat est.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc
+          efficitur purus vel diam dignissim tempus. Pellentesque ut euismod
+          dui. In id egestas mi. Quisque placerat sem leo, et iaculis nunc
+          molestie quis. Quisque consequat condimentum enim ac feugiat. Ut
+          euismod efficitur fermentum. Pellentesque convallis rhoncus ex ut
+          porta. Praesent at justo nibh. Aenean consequat purus nec diam porta,
+          et eleifend elit dapibus.
+        </p>
+      </section>
+      <section>
+        <img></img>
+      </section>
     </main>
   );
 };
