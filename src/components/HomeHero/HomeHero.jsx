d33b194b843acabbf1f0034167aa34636010d9ba
rouenemedina <rouenemedina@gmail.com>
2024-09-13 01:10:43 -0400

    added the profile image

--- src/components/HomeHero/HomeHero.jsx
+++ src/components/HomeHero/HomeHero.jsx
@@ -1,14 +1,25 @@
 import "./HomeHero.scss";
 import React from "react";
+import Profile from "../../assets/images/DSC00821-Edit.png";
 
 const HomeHero = () => {
   return (
     <>
       <main className="hero">
-        <img></img>
-        <h1>ROUENE MEDINA</h1>
-        <h2>Full Stack Software Developer</h2>
-        <h3>based in Toronto, Ontario</h3>
+        <section className="hero__details">
+          <h1>ROUENE MEDINA</h1>
+          <article>
+            <h2>Full Stack Software Developer</h2>
+            <h3>based in Toronto, Ontario</h3>
+          </article>
+        </section>
+        <section className="hero__profile">
+          <img
+            src={Profile}
+            alt="Rouene Medina's photo"
+            className="hero__img"
+          ></img>
+        </section>
       </main>
     </>
   );
--- src/components/HomeSkills/HomeSkills.jsx
+++ src/components/HomeSkills/HomeSkills.jsx
@@ -5,6 +5,20 @@ const HomeSkills = () => {
   return (
     <main className="skills">
       <h3>My Skills</h3>
+      <section>
+        <article>
+          <h4>FRONTEND</h4>
+          <img></img>
+        </article>
+        <article>
+          <h4>BACKEND</h4>
+          <img></img>
+        </article>
+        <article>
+          <h4>DATABASE</h4>
+          <img></img>
+        </article>
+      </section>
     </main>
   );
 };
