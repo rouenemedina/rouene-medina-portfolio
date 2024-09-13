215a118597c66b14714b0354e2d724641b852a08
rouenemedina <rouenemedina@gmail.com>
2024-09-13 02:29:17 -0400

    created a template for the skills and added styling

--- src/components/HomeSkills/HomeSkills.jsx
+++ src/components/HomeSkills/HomeSkills.jsx
@@ -4,43 +4,61 @@ import React from "react";
 const HomeSkills = () => {
   return (
     <main className="skills">
-      <h3>My Skills</h3>
-      <section>
-        <article>
-          <h4>PROGRAMMING LANGUAGES</h4>
-          <img></img>
+      <h3 className="skills__title">My Skills</h3>
+      <section className="skills__container">
+        <article className="skills__card">
+          <h4 className="skills__subtitle">PROGRAMMING LANGUAGES</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
-        <article>
-          <h4>LIBRARIES AND FRAMEWORKS</h4>
-          <img></img>
+        <article className="skills__card">
+          <h4 className="skills__subtitle">LIBRARIES AND FRAMEWORKS</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
-        <article>
-          <h4>DATABASE</h4>
-          <img></img>
+        <article className="skills__card">
+          <h4 className="skills__subtitle">DATABASE</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
-        <article>
-          <h4>WEB SERVICES</h4>
-          <img></img>
+        <article className="skills__card">
+          <h4 className="skills__subtitle">WEB SERVICES</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
-        <article>
-          <h4>API TESTING</h4>
-          <img></img>
+        <article className="skills__card">
+          <h4 className="skills__subtitle">API TESTING</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
-        <article>
-          <h4>VERSION CONTROL</h4>
-          <img></img>
+        <article className="skills__card">
+          <h4 className="skills__subtitle">VERSION CONTROL</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
-        <article>
-          <h4>DEVELOPMENT TOOLS</h4>
-          <img></img>
+        <article className="skills__card">
+          <h4 className="skills__subtitle">DEVELOPMENT TOOLS</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
-        <article>
-          <h4>DESIGN AND PROTOTYPING</h4>
-          <img></img>
+        <article className="skills__card">
+          <h4 className="skills__subtitle">DESIGN AND PROTOTYPING</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
-        <article>
-          <h4>PROJECT MANAGEMENT</h4>
-          <img></img>
+        <article className="skills__card">
+          <h4 className="skills__subtitle">PROJECT MANAGEMENT</h4>
+          <div className="skills__subcard">
+            <img></img>
+          </div>
         </article>
       </section>
     </main>
