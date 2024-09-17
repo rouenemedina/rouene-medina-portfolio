dfd0dd9ebe7ae6713eb6ac458d53447f9c91f36a
rouenemedina <rouenemedina@gmail.com>
2024-09-17 15:58:02 -0400

    added styling to hero and proejcts sections

--- src/components/HomeProjects/HomeProjects.jsx
+++ src/components/HomeProjects/HomeProjects.jsx
@@ -46,6 +46,46 @@ const HomeProjects = () => {
             </Buttons>
           </div>
         </article>
+        <article className="projects__card">
+          <div className="projects__subcard">
+            <img
+              src={photonest}
+              alt="PhotoNest Media Queries"
+              className="projects__img"
+            ></img>
+          </div>
+          <div className="projects__details">
+            <h4 className="projects__subtitle">PhotoNest v2.0</h4>
+            <p className="projects__description">
+              Welcome to the second version of PhotoNest, a responsive
+              application designed for photographers to showcase their work and
+              connect with clients. This version has been entirely rewritten in
+              TypeScript to enhance code reliability and maintainability. 
+            </p>
+          </div>
+          <div className="projects__buttons">
+            <Buttons type="button" className="projects__btn">
+              <a
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+                className="projects__link"
+              >
+                DEMO: COMING SOON!
+              </a>
+            </Buttons>
+            <Buttons type="button" className="projects__btn">
+              <a
+                href="https://github.com/rouenemedina/rouene-medina-photonest-v2"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="projects__link"
+              >
+                GITHUB
+              </a>
+            </Buttons>
+          </div>
+        </article>
       </section>
     </main>
   );
