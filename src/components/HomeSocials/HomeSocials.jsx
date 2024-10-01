0027bb6ba634c0bcd5209eb32072f1b74ad05e70
rouenemedina <rouenemedina@gmail.com>
2024-10-01 11:21:05 -0400

    added assets for socials, styling the socials component for all media queries

--- src/components/HomeSocials/HomeSocials.jsx
+++ src/components/HomeSocials/HomeSocials.jsx
@@ -1,28 +1,29 @@
 import "./HomeSocials.scss";
 import React from "react";
-import linkedin from "../../assets/images/DSC01184-Edit.png";
-import github from "../../assets/images/DSC01214.jpg";
+import mail from "../../assets/images/mail.jpg";
+import linkedin from "../../assets/images/linkedin.png";
+import github from "../../assets/images/github.jpg";
 
 const HomeSocials = () => {
   return (
     <main className="socials">
-      <h3 className="socials__title">Let's Connect</h3>
+      <h2 className="socials__title">Let's Connect</h2>
       <section className="socials__container">
         <article className="socials__card">
-          <img alt="Rouene's Email" className="socials__img"></img>
-          <a className="socials__link">
+          <img src={mail} alt="Rouene's Email" className="socials__img"></img>
+          <a href="mailto:rouenemedina@gmail.com" className="socials__link">
             <h3 className="socials__subtitle">MAIL</h3>
           </a>
         </article>
         <article className="socials__card">
           <img src={linkedin} alt="Rouene's Linkedin" className="socials__img"></img>
-          <a className="socials__link">
+          <a href="https://www.linkedin.com/in/rouenemedina/" target="_blank" rel="noopener noreferrer" className="socials__link">
             <h3 className="socials__subtitle">LINKEDIN</h3>
           </a>
         </article>
         <article className="socials__card">
           <img src={github} alt="Rouene's Github" className="socials__img"></img>
-          <a className="socials__link">
+          <a href="https://github.com/rouenemedina" target="_blank" rel="noopener noreferrer" className="socials__link">
             <h3 className="socials__subtitle">GITHUB</h3>
           </a>
         </article>
