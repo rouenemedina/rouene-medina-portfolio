0e6bb4d7a75067fe243c9d2a8b9d924d555fe46f
rouenemedina <rouenemedina@gmail.com>
2024-10-02 14:53:26 -0400

    styled projects and socials component for all media queries

--- src/components/HomeSocials/HomeSocials.jsx
+++ src/components/HomeSocials/HomeSocials.jsx
@@ -10,20 +10,38 @@ const HomeSocials = () => {
       <h2 className="socials__title">Let's Connect</h2>
       <section className="socials__container">
         <article className="socials__card">
-          <img src={mail} alt="Rouene's Email" className="socials__img"></img>
           <a href="mailto:rouenemedina@gmail.com" className="socials__link">
+            <img src={mail} alt="Rouene's Email" className="socials__img"></img>
             <h3 className="socials__subtitle">MAIL</h3>
           </a>
         </article>
         <article className="socials__card">
-          <img src={linkedin} alt="Rouene's Linkedin" className="socials__img"></img>
-          <a href="https://www.linkedin.com/in/rouenemedina/" target="_blank" rel="noopener noreferrer" className="socials__link">
+          <a
+            href="https://www.linkedin.com/in/rouenemedina/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socials__link"
+          >
+            <img
+              src={linkedin}
+              alt="Rouene's Linkedin"
+              className="socials__img"
+            ></img>
             <h3 className="socials__subtitle">LINKEDIN</h3>
           </a>
         </article>
         <article className="socials__card">
-          <img src={github} alt="Rouene's Github" className="socials__img"></img>
-          <a href="https://github.com/rouenemedina" target="_blank" rel="noopener noreferrer" className="socials__link">
+          <a
+            href="https://github.com/rouenemedina"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socials__link"
+          >
+            <img
+              src={github}
+              alt="Rouene's Github"
+              className="socials__img"
+            ></img>
             <h3 className="socials__subtitle">GITHUB</h3>
           </a>
         </article>
