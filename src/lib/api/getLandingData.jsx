cec96bb1574f794e9e5fb6d62d58373960408567
rouenemedina <rouenemedina@gmail.com>
2024-11-22 20:21:21 -0500

    transferred db to data

--- src/lib/api/getLandingData.jsx
+++ src/lib/api/getLandingData.jsx
@@ -2,14 +2,16 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const getLandingData = async() => {
-    try {
-        const response = await axios.get(`${API_URL}/landing`);
-        return response.data.data;
-    } catch(err) {
-        console.log("Error fetching data",err);
-        return null;
-    }
+const getLandingData = async () => {
+  try {
+    console.log("API URL:", `${API_URL}/landing`);
+    const response = await axios.get(`${API_URL}/landing`);
+    console.log("Response:", response);
+    return response.data.data;
+  } catch (err) {
+    console.log("Error fetching data", err || err.message);
+    return null;
+  }
 };
 
 export default getLandingData;
